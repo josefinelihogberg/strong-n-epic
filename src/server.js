c1ef05085b15c48674c1838f1f7d7beea93aa484
Josefine <leexue417@gmail.com>
2023-09-11 14:44:23 +0200

    updated Model

--- src/server.js
+++ src/server.js
@@ -1,89 +1,81 @@
-import { createServer } from "miragejs";
-
-let users = [
-  { id: 1, username: "Bob", role: "ADMIN", password: "123" },
-  { id: 2, username: "Adam", role: "USER", password: "123", activities: ["boxing", "yoga"] },
-  {
-    id: 3,
-    username: "Yves",
-    role: "USER",
-    password: "123",
-    activities: ["spinning", "bicycle", "yoga"],
-  },
-];
-
-let activities = [
-  {
-    id: 1,
-    title: "boxing",
-    coach: "Erik Eriksson",
-    day: "Monday",
-    created: "20230907",
-    time: "18:00",
-    description: "an amazing activity for you who wants to become strong",
-  },
-
-  {
-    id: 2,
-    title: "yoga",
-    coach: "Emelie Johansson",
-    day: "Tuesday",
-    created: "20230910",
-    time: "15:00",
-    description: "an amazing activity for you who wants to relax",
-  },
-  {
-    id: 3,
-    title: "spinning bicycle",
-    coach: "Ulf Andersson",
-    day: "Wednesday",
-    created: "20230907",
-    time: "18:00",
-    description: "an amazing activity for you who wants to become strong",
-  },
-];
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    user: Model,
+    activity: Model,
+  },
+
   routes() {
     this.namespace = "api";
 
+    this.get("/admin/users", (schema, request) => {
+      return schema.users.all();
+    });
+
+    this.get("/admin/activities", (schema, request) => {
+      return schema.activities.all();
+    });
+
     this.post("/users", (schema, request) => {
       let attrs = JSON.parse(request.requestBody);
-      attrs.id = Math.floor(Math.random() * 100);
-
-      users.push(attrs);
-
-      return { user: attrs };
+      return schema.users.create(attrs);
     });
 
-    this.post("/admin/activity", (schema, request) => {
+    this.post("/admin/activities", (schema, request) => {
       let attrs = JSON.parse(request.requestBody);
-      attrs.id = Math.floor(Math.random() * 100);
-
-      activities.push(attrs);
-
-      return { activity: attrs };
-    });
-
-    this.get("/admin/users", () => {
-      return users;
-    });
-
-    this.get("admin/activities", () => {
-      return activities;
+      return schema.activities.create(attrs);
     });
 
     this.delete("/admin/activities/:id", (schema, request) => {
       let id = request.params.id;
-      let activity = schema.activities.find(id);
+      return schema.activities.find(id).destroy();
+    });
+  },
 
-      if (!activity) {
-        return new Response(404, {}, { message: "Activity not found" });
-      }
+  seeds(server) {
+    server.create("user", { id: 1, username: "Bob", role: "ADMIN", password: "123" });
+    server.create("user", {
+      id: 2,
+      username: "Adam",
+      role: "USER",
+      password: "123",
+      activities: ["boxing", "yoga"],
+    });
+    server.create("user", {
+      id: 3,
+      username: "Yves",
+      role: "USER",
+      password: "123",
+      activities: ["spinning", "bicycle", "yoga"],
+    });
 
-      activity.destroy();
-
-      return { activity: id };
+    server.create("activity", {
+      id: 1,
+      title: "boxing",
+      coach: "Erik Eriksson",
+      day: "Monday",
+      created: "20230907",
+      time: "18:00",
+      description: "an amazing activity for you who wants to become strong",
+    });
+    server.create("activity", {
+      id: 2,
+      title: "yoga",
+      coach: "Emelie Johansson",
+      day: "Tuesday",
+      created: "20230910",
+      time: "15:00",
+      description: "an amazing activity for you who wants to relax",
+    });
+    server.create("activity", {
+      id: 3,
+      title: "spinning bicycle",
+      coach: "Ulf Andersson",
+      day: "Wednesday",
+      created: "20230907",
+      time: "18:00",
+      description: "an amazing activity for you who wants to become strong",
     });
   },
 });
